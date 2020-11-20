b6a46604169019372b9670458d5e222f76b24375
Vitor <vbardasson@id.uff.br>
2020-11-20 18:21:05 -0300

    Geração da tabela verdade alterada

--- main.hs
+++ main.hs
@@ -16,16 +16,17 @@ main = do
     let parsedFormula = removeUnusedChars formula
     let finalParsedFormula = addExternalParenthesis parsedFormula
     let matchingParenthesisList = getMatchingParenthesis finalParsedFormula
-    print(matchingParenthesisList)
-    print(finalParsedFormula)
+    --print(matchingParenthesisList)
+    --print(finalParsedFormula)
     let subFormulas = sliceSubFormulas finalParsedFormula matchingParenthesisList
-    print subFormulas
+    --print subFormulas
     let varList = [[x]|x<-finalParsedFormula, x `elem` ['a'..'z'] || x `elem` ['A'..'Z']]
-    let l = [binaryList(toBinary x) (length varList) | x <- [0..2^(length (varList))-1]]
-    let truthTable = reverseList l
-    print(truthTable)
+    let truthTable = [binaryList(toBinary x) (length varList) | x <- [0..2^(length (varList))-1]]
+    let l = reverseList truthTable
+    let parsedTT = [parseTTLine x | x<-l]
     let entries = nub varList ++ subFormulas --CONSERTAR OPERADOR (->) NO CABEÇALHO
     print entries
+    print parsedTT
 
 removeUnusedChars :: [Char] -> [Char]
 removeUnusedChars formula = [c | c <- formula, c /= ' ', c /='>']
@@ -62,6 +63,9 @@ resolve (Disjuncao a b) xs = (resolve a xs) || (resolve b xs)
 resolve (Implicacao a b) xs = not(resolve a xs) || (resolve b xs)
 resolve (Negacao a Empty) xs = not(resolve a xs) 
 
+parseTTLine line = [toBool x|x<-line]
+toBool 0 = False
+toBool 1 = True
 operators = [(Implicacao, '-'), (Disjuncao, '|'), (Conjuncao, '&'), (Negacao, '~')] --OPERADORES SUPORTADOS
 --RESOLVER RECURSIVAMENTE AS FÓRMULAS A ESQUERDA E A DIREITA, P/CADA SUBFORMULA EM subFormulas
 
