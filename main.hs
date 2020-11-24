262f07664f71673a1230a9bdfb5c3822d3cd6b3a
Vitor <vbardasson@id.uff.br>
2020-11-23 21:19:15 -0300

    Geração da tabela verdade funcionando corretamente para casos simples

--- main.hs
+++ main.hs
@@ -4,49 +4,48 @@ import Data.Maybe
 --import Data.Typeable
 
 type Letra = Char
-
+--data Operation = Conjuncao | Disjuncao | Implicacao | Negacao deriving Enum
 data Formula =
-  T | F | Var Char | Val Bool 
+  Var Char | Val Bool 
   | Conjuncao Formula Formula
   | Disjuncao Formula Formula
-  | Negacao Formula Formula
   | Implicacao Formula Formula
+  | Negacao Formula Formula
   | Empty
-
-
+  deriving Show
+  
 main = do 
     putStrLn "Digite a fórmula:"
     formula <- getLine
     let parsedFormula = removeUnusedChars formula
     let finalParsedFormula = addExternalParenthesis parsedFormula
     let matchingParenthesisList = getMatchingParenthesis finalParsedFormula
-    --print(matchingParenthesisList)
-    --print(finalParsedFormula)
     let subFormulas = sliceSubFormulas finalParsedFormula matchingParenthesisList
-    --print subFormulas
-    let varList = [ x |x<-finalParsedFormula, x `elem` ['a'..'z'] || x `elem` ['A'..'Z']]
+    let varList = [[x] |x<-finalParsedFormula, x `elem` ['a'..'z'] || x `elem` ['A'..'Z']]
     let truthTable = [binaryList(toBinary x) (length varList) | x <- [0..2^(length (varList))-1]]
     let l = reverseList truthTable
     let parsedTT = [parseTTLine x | x<-l]
-    --let tempVarList = map toChar varList
     let tt = [zip varList x | x<-parsedTT]
-    --no momento, passando 
-    --getop char bool bool
-    let aux_pairs = [resolve (getOperation (finalParsedFormula!!1) (Val (fromJust(lookup (finalParsedFormula!!2) x))) (Val (fromJust(lookup (finalParsedFormula!!3) x)))) x |x<-tt]
+    --let test = [resolve(stringToFormula (finalParsedFormula!!0) finalParsedFormula x) | x<-tt]
+    --let a = stringToFormula (finalParsedFormula!!1) finalParsedFormula
+    let solved = [resolve(stringToFormula (finalParsedFormula!!1) finalParsedFormula) (x)|x<-tt]
+    print solved
     print tt
-    print aux_pairs
+    print subFormulas
+    
+--botar variaveis na tabela verdade
+getValue :: [Char] -> [([Char], Bool)] -> Bool
+getValue str tt =  fromJust(lookup str tt)
 
---toChar:: [Char] -> Char
---toChar c = c!!0
-
---toFormula :: String -> Formula 
-
+--AINDA ESTÁ LIDANDO APENAS COM FÓRMULAS SIMPLES
+stringToFormula :: Char -> [Char] -> Formula
+stringToFormula '&' str = Conjuncao (Var (str!!2)) (Var (str!!3))
+stringToFormula '-' str = Implicacao (Var (str!!2)) (Var (str!!3))
+stringToFormula '|' str = Disjuncao (Var (str!!2)) (Var (str!!3))
+stringToFormula '~' str = Negacao (Var(str!!2)) Empty
 
 removeUnusedChars :: [Char] -> [Char]
 removeUnusedChars formula = [c | c <- formula, c /= ' ', c /='>']
-
-getCharOnIndex :: String -> Int -> Char
-getCharOnIndex str i = str!!i
 
 getMatchingParenthesis :: String -> [(Int, Int)]
 getMatchingParenthesis = aux 0 []
@@ -71,18 +70,11 @@ toBinary n = mod n 2:toBinary(div n 2)
 reverseList [] = []
 reverseList (x:xs) = reverseList xs ++ [x]
 
---getOperation :: Char -> Formula -> Formula -> Formula
-getOperation :: Char -> Formula -> Formula -> Formula
-getOperation '&' a b = (Conjuncao a b)
-getOperation '|' a b = (Disjuncao a b)
-getOperation '~' a b = (Negacao a Empty)
-getOperation '-' a b = (Implicacao a b)
-
 sliceSubFormulas :: [a] -> [(Int, Int)] -> [[a]]
 sliceSubFormulas formula matchingParenthesis = [take ((snd x)-(fst x)-1) (drop (fst x+1) formula) | x <- matchingParenthesis]
 
-resolve :: Formula -> [(Letra,Bool)] -> Bool --RECEBE A SUBFÓRMULA COM AS LETRAS, ONDE CADA LETRA VAI TER O SEU VALOR E RETORNARÁ UM BOOL
-resolve (Var v) bs      = fromJust (lookup v bs)
+resolve :: Formula -> [([Char],Bool)] -> Bool  --a corrigir
+resolve (Var v) bs      = fromJust(lookup [v] bs)
 resolve (Conjuncao a b) xs = (resolve a xs) && (resolve b xs)
 resolve (Disjuncao a b) xs = (resolve a xs) || (resolve b xs)
 resolve (Implicacao a b) xs = not(resolve a xs) || (resolve b xs)
@@ -91,8 +83,5 @@ resolve (Negacao a Empty) xs = not(resolve a xs)
 parseTTLine line = [toBool x|x<-line]
 toBool 0 = False
 toBool 1 = True
-operators = [(Implicacao,'-'), (Disjuncao,'|'), (Conjuncao,'&'), (Negacao,'~')] --OPERADORES SUPORTADOS
---RESOLVER RECURSIVAMENTE AS FÓRMULAS A ESQUERDA E A DIREITA, P/CADA SUBFORMULA EM subFormulas
+--operators = [(Implicacao,'-'), (Disjuncao,'|'), (Conjuncao,'&'), (Negacao,'~')] --OPERADORES SUPORTADOS
 
--- generateAuxList :: [[Char]] -> [[Char]] -> [Bool] -> [Char] -> (String,Bool)
--- generateAuxList varList subFormulas truthTable x = 
